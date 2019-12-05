ed4277f222b10f47629a52906bd8d7c43d39f1c0
Paweł Dworniczak <dworniczakpawel@gmail.com>
2019-12-05 21:46:55 +0100

    add firestore rules and indexes, prepare dev config file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,9 @@
 import firebase from 'firebase';
 import firebaseConfig from 'firebaseConfig.json';
+import firebaseConfigDev from 'firebaseConfig.dev.json';
 import { environment } from './environments/environment';
 
-console.log(environment.production);
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(environment.production ? firebaseConfig : firebaseConfigDev);
 const db = firebase.firestore();
 
 export default firebase;
